8ad90a23de50da9672a425416e2984c66f24f9a2
CHRYS10 <chma3198@colorado.edu>
2021-12-05 19:40:23 -0700

    LandrumMalloyTran Final Project Upload

--- CalmingScenes/Assets/Scenes/SceneSwitcher.cs
+++ CalmingScenes/Assets/Scenes/SceneSwitcher.cs
@@ -3,6 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
+    public GameObject Blue;
+    public GameObject Green;
+    public GameObject Purple;
     public void GotoGong()
     {
         SceneManager.LoadScene("Landrum");
@@ -21,5 +24,26 @@ public class SceneSwitcher : MonoBehaviour
     public void GotoMenu()
     {
         SceneManager.LoadScene("Menu");
+    }
+    public void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("Trigger occurred");
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (collision.gameObject.name == Blue.name)
+        {
+            GotoGong();
+            Debug.Log("Landrum Success Load!");
+
+        }
+        if (collision.gameObject.name == Green.name)
+        {
+            GotoStars();
+            Debug.Log("Tran Success Load!");
+        }
+        if (collision.gameObject.name == Purple.name)
+        {
+            GotoCandle();
+            Debug.Log("Malloy Success Load!");
+        }
     }
 }
