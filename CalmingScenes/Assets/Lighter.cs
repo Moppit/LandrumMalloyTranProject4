ba213505c9f8ca7e5df5ac876f3b7cce64f2c5ef
CHRYS10 <chma3198@colorado.edu>
2021-12-05 17:29:06 -0700

    Malloy Scene Combine

---
+++ CalmingScenes/Assets/Lighter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lighter : MonoBehaviour
+{
+    [SerializeField] private GameObject Flame;
+    //public GameObject MatchFlame;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Match")
+        {
+            //print("trigger success");
+            Flame.SetActive(true);
+            //MatchFlame.SetActive(false);
+        }
+        
+    }
+    
+}
